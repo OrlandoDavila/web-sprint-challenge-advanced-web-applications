8f94ad41e2503df3aaad78056f7e81b576175388
Orlando Davila <odavila0308@gmail.com>
2020-08-21 13:36:27 -0400

    ugh, this test!

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,50 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import BubblePage from "./BubblePage";
+import React from 'react';
+import BubblePage from '../components/BubblePage';import {render} from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 
-test("Fetches data and renders the bubbles", () => {
-  // Finish this test
+
+
+const colorData =  [
+  {
+    color: 'aliceblue',
+    code: {
+      hex: '#f0f8ff'
+    },
+    id: 1
+  },
+  {
+    color: 'limegreen',
+    code: {
+      hex: '#99ddbc'
+    },
+    id: 2
+  },
+  {
+    color: 'aqua',
+    code: {
+      hex: '#00ffff'
+    },
+    id: 3
+  },
+  {
+    color: 'aquamarine',
+    code: {
+      hex: '#7fffd4'
+    },
+    id: 4
+  },
+];
+
+jest.mock(mockGetColors())
+test("Fetches data and renders the bubbles", async () => {
+
+  mockGetColors(colorData)
+  const { getByText } = render(<BubblePage />);
+  const colors = getByText(/colors/i);
+  expect(colors).toBeInTheDocument();
+
+  const bubbles = getByText(/bubbles/i);
+  expect(bubbles).toBeInTheDocument();
+
 });
+// Finish the test in `BubblePage.test.js` to test that your app is fetching the bubble data from the API
